3f11e33bd02cf531bd2a7d8b08e94cb3e27d68d8
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-21 16:09:46 +0530

    n

--- scm/src/main/java/com/scm/scm/repository/RoleRepository.java
+++ scm/src/main/java/com/scm/scm/repository/RoleRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);  // return Optional<Role> instead of Role
+    boolean existsByName(String name);
 }
