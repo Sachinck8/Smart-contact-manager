c8a94cf665635568a7a229f128b9e5457a1db6ee
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-21 11:10:20 +0530

    controller

--- scm/src/main/java/com/scm/scm/controller/AuthController.java
+++ scm/src/main/java/com/scm/scm/controller/AuthController.java
@@ -6,16 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class AuthController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
+    
 
     public AuthController(UserService userService, ) {
         this.userService = userService;
-        this.passwordEncoder=passwordEncoder;
+        
     }
 
     
