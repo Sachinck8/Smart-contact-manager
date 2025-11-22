2957ac3a072297fa436a1f422f6a4bd189c358b1
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-22 13:10:46 +0530

    dashboard

--- scm/src/main/java/com/scm/scm/controller/DashboardController.java
+++ scm/src/main/java/com/scm/scm/controller/DashboardController.java
@@ -8,6 +8,6 @@ public class DashboardController {
 
     @GetMapping("/dashboard")
     public String dashboard() {
-        return "user/dashboard";   // ⭐ load from /templates/user/dashboard.html
+        return "user/fragements/dashboard";   // ⭐ load from /templates/user/dashboard.html
     }
 }
