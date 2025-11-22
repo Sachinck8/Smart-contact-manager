1cdcaddd38a401cc5f287e971438e167cee8cfab
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-22 12:08:00 +0530

    dashborad

---
+++ scm/src/main/java/com/scm/scm/controller/dashboardController.java
@@ -0,0 +1,13 @@
+package com.scm.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "user/dashboard";   // ⭐ load from /templates/user/dashboard.html
+    }
+}
