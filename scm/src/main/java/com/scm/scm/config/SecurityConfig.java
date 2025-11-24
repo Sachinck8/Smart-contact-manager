794f8ddb89169491c4ab2f96148005c04ae57937
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-24 15:18:00 +0530

    registerdb

--- scm/src/main/java/com/scm/scm/config/SecurityConfig.java
+++ scm/src/main/java/com/scm/scm/config/SecurityConfig.java
@@ -7,38 +7,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
 
-    // Render HTTPS support
     @Bean
     public ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
     }
 
-    // Password encoder
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // Security filter chain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/register").permitAll()
-                .anyRequest().authenticated()
-)
-
+                        .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .loginProcessingUrl("/login")   // IMPORTANT
-                        .usernameParameter("email")     // VERY IMPORTANT
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
                         .passwordParameter("password")
                         .defaultSuccessUrl("/dashboard", true)
                         .failureUrl("/login?error=true")
